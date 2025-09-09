9f0ea26c327c858115d97e5e438a4c4191bb8ee6
lucasfvera <lucasferreyravera@gmail.com>
2025-09-08 21:58:20 -0300

    Add valid message to avoid layout shift

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,4 +1,5 @@
 export const ErrorMessage = ({ errors }: { errors: string[] }) => {
+    if (!errors.length) return <p className="text-green-700">Valid</p>;
     return errors.map((e, i) => (
         <p key={i} className="text-red-700">
             {e}
